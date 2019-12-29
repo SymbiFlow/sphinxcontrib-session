bef3da271c41365c59b190e1094991fe81505952
Tim 'mithro' Ansell <me@mith.ro>
2019-12-29 02:27:33 +0100

    Rework to make things like highlighting work.

--- sphinxcontrib_session/__init__.py
+++ sphinxcontrib_session/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding: utf-8
 
 import re
@@ -13,57 +13,36 @@ from sphinx.errors import ExtensionError
 from sphinx.directives.code import CodeBlock
 
 import pygments
+from pygments import token
 from pygments.token import Token
 from pygments.lexers import get_lexer_by_name, guess_lexer
 from pygments.formatters import HtmlFormatter
 
 
-class SessionDirective(CodeBlock):
 
-    def run(self):
-        n = session()
-        n.contains = CodeBlock.run(self)
-        return [n]
+# --------------------------
 
-
-class session(nodes.Element): pass
-
-
-def token_is_whitespace(token):
-    token_type, token_text = token
-    if token_type is not Token.Text:
-        return False
-    return not token_text.strip()
+def rewrite_match(m):
+    klass, content, extra_whitespace = m.groups()
+    #content = html.escape(content)
+    #extra_whitespace = html.escape(extra_whitespace)
+    return '<span class="{}" data-content="{}{}"></span>'.format(
+        klass, content, extra_whitespace)
 
 
-span_re = re.compile('^(<span[^>]*)>(.*?)</span>(.*?)$', re.DOTALL)
+SPAN_REGEX = {}
 
 
-def to_data_content(token, i):
-    if not i.strip():
-        return i
+def rewrite_span(s, t):
+    if t not in SPAN_REGEX:
+        klass = token.STANDARD_TYPES[t]
 
-    m = span_re.match(i)
-    assert m, repr(i)
+        SPAN_REGEX[t] = re.compile('<span class="({})">([^<]*)</span>(\\s+)'.format(klass), re.DOTALL)
 
-    # FIXME: Why is this needed...
-    # Append an extra newline if needed.
-    token_type, token_text = token
-    if token_text.endswith('\n'):
-        extra = '\n'
-    else:
-        extra = ''
+    regex = SPAN_REGEX[t]
+    return regex.sub(rewrite_match, s)
 
-    return '{} data-content="{}"></span>'.format(m.group(1), m.group(2)+extra+m.group(3))
-
-
-def fix_token_newline(token, i):
-    # FIXME: Why is this needed?
-    token_type, token_text = token
-    if not token_text.endswith('\n') and i.endswith('\n'):
-        i = i[:-1]
-    return i
-
+# --------------------------
 
 SESSION_TYPES = {
     'BashLexer':        'shell-session',
@@ -93,75 +72,73 @@ def session_lexer(lexer):
 
     return None
 
-
-def visit_session_html(self, node):
-    formatter = self.highlighter.get_formatter(nowrap=True)
-
-    if not hasattr(node, 'contains'):
-        print("Warning no contents found on", node)
-        return
-
-    data_node = node.contains[0]
-
-    content = data_node.rawsource
-
-    lang = data_node.get('language', 'auto')
-    if lang in ('auto', 'default'):
-        lexer_orig = guess_lexer(content)
-    else:
-        lexer_orig = get_lexer_by_name(lang)
+def get_lexer(self, source, lang, opts=None, force=False, location=None):
+    lexer = XXXX.get_lexer(self, source, lang, opts, force, location)
 
     lexer = session_lexer(lexer_orig)
     if lexer is None:
-        print("""\
+        logger.warning("""\
 Session contents should be a XXXSession lexer like `console`, `doscon` or \
-`ps1con`. Got instead '{}'.""".format(lexer_orig))
+`ps1con`. Got instead '{}'.""".format(lexer_orig), location=location)
         return
+    return lexer
+
+# --------------------------
+
+class SessionDirective(CodeBlock):
+
+    def run(self):
+        n = session()
+        n.contains = CodeBlock.run(self)
+        return [n]
+
+
+class session(nodes.Element): pass
+
+# --------------------------
+
+def visit_session_html(self, node):
+
+    if not hasattr(node, 'contains'):
+        logger.warning(
+            "Warning no contents found on {}".format(node),
+            location=location)
+        return
+    data_node = node.contains[0]
+
+    if data_node.rawsource != data_node.astext():
+        # most probably a parsed-literal block -- don't highlight
+        return super().visit_literal_block(date_node)
 
     lang = data_node.get('language', 'default')
     linenos = data_node.get('linenos', False)
     highlight_args = data_node.get('highlight_args', {})
+    highlight_args['force'] = data_node.get('force', False)
+    if lang is self.builder.config.highlight_language:
+        # only pass highlighter options for original language
+        opts = self.builder.config.highlight_options
+    else:
+        opts = {}
 
-    output = []
-    output.append(self.starttag(
-        node, 'div', suffix='', CLASS='highlight-%s notranslate' % lang))
-    output.append(self.starttag(node, 'div', suffix='', CLASS='highlight'))
-    output.append(self.starttag(node, 'pre'))
+    highlighted = self.highlighter.highlight_block(
+        data_node.rawsource, lang, opts=opts, linenos=linenos,
+        location=(self.builder.current_docname, data_node.line),
+        **highlight_args
+    )
 
-    tokens = list(pygments.lex(content, lexer))
+    o = highlighted
+    for t in [Token.Generic.Prompt, Token.Generic.Output]:
+        o = rewrite_span(o, t)
 
-    token_groups = [[tokens.pop(0),]]
-    for token in tokens:
-        current_group = token_groups[-1]
-
-        # Is this just a whitespace token?
-        if token_is_whitespace(token) and current_group[-1][0] is Token.Generic.Prompt:
-            current_group.append(token)
-        else:
-            token_groups.append([token])
-
-    while len(token_groups) > 0:
-        current_tokens = token_groups.pop(0)
-
-        o = pygments.format(current_tokens, formatter)
-        i = fix_token_newline(current_tokens[-1], o)
-
-        if current_tokens[0][0] in (Token.Generic.Prompt, Token.Generic.Output):
-            i = to_data_content(current_tokens[-1], i)
-
-        #print(current_tokens, '\n', repr(o), '\n', repr(i), '\n')
-        output.append(i)
-
-    output.append('</pre>')
-    output.append('</div>')
-    output.append('</div>')
-    self.body.append("".join(output))
+    starttag = self.starttag(
+        node, 'div', suffix='', CLASS='highlight-%s notranslate' % lang)
+    self.body.append(starttag + o + '</div>\n')
     raise nodes.SkipNode
-
 
 def depart_session_html(self, node):
     return
 
+# --------------------------
 
 def _is_html(app):
     return app.builder.name in ('html', 'readthedocs')
@@ -179,6 +156,7 @@ def copy_stylesheet(app, exception):
     dest = os.path.join(app.builder.outdir, '_static', 'session.css')
     copyfile(os.path.join(source, "session.css"), dest)
 
+# --------------------------
 
 def setup(app):
     app.connect('builder-inited', add_stylesheet)
